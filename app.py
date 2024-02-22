e67d34586673de7d99e260ad316f3d0d78f137ef
André Luiz Albano <andre_barros@id.uff.br>
2024-02-22 13:54:43 -0300

    Initial commit
    This initial commit creates two html files, the app.py file from Flask, and the main.py file to run the pywebview.

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+from flask_cors import CORS
+
+app = Flask(__name__, template_folder='layouts')
+CORS(app)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+if __name__ == "__main__":
+    app.run(port=5000)
