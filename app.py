01ba837b96b38e5035ab431842e5d44fd1b685ce
André Luiz Albano <andre_barros@id.uff.br>
2024-03-01 19:29:03 -0300

    added pytesseract

--- app.py
+++ app.py
@@ -4,14 +4,12 @@ from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS
 from litologias import *
 
-
 app = Flask(__name__, template_folder='layouts')
 CORS(app)
 
 @app.route('/')
 def index():
     return render_template('index.html')
-
 
 @app.route('/openFile')
 def openFile():
@@ -48,8 +46,6 @@ def get_wells():
 @app.route('/litologias')
 def litologias():
     return lithologies_name
-
-
 
 # example
 # @app.route('/about')
